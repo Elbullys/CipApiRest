22758ecab8df12119ffef3a2b3f58dfbe5848ed2
Elbullys <hectorjosueoficial@gmail.com>
2025-12-03 16:46:35 -0600

    commit prueba cookies node1

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const NodeCache = require('node-cache');
+
 module.exports = {
     app: {
         port: process.env.PORT || 4000,
@@ -11,11 +11,7 @@ module.exports = {
         database: process.env.DBNAME || 'db'
     },
 
-    my_cache: new NodeCache({
-        stdTTL: 300,  // Tiempo de vida por defecto en segundos
-        checkperiod: 60  // Intervalo de revisión en segundos
-    }),
-    cacheKey: 'cacheConsRetTransito',
+   
     salt_rounds: process.env.SALT_ROUNDS || 20,
     secret_jwt_key: process.env.SECRET_JWT_KEY,
     secure_cookie: process.env.SECURE_COOKIE === 'true',
